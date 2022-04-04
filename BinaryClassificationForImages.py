38a4f5cbbf3a83e239f79dc7b9bed287d3c82781
rilopez99316 <ilbroberto99316@gmail.com>
2022-04-04 13:57:52 -0600

    loading data from fashion MNIST

--- BinaryClassificationForImages.py
+++ BinaryClassificationForImages.py
@@ -11,4 +11,22 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import tensorflow as tf
 from tensorflow import keras
 from keras import metrics
+from keras.datasets import fashion_mnist
+
+# Load the Fashion MNIST dataset.
+(X_train, Y_train), (X_test, Y_test) = fashion_mnist.load_data()
+
+# Flatten Y_train and Y_test, so they become vectors of label values.
+# The label for X_train[0] is in Y_train[0].
+Y_train = Y_train.flatten()
+Y_test = Y_test.flatten()
+
+np.random.seed(0) # For reproducibility purposes
+
+# Shuffle the order of the training examples.
+indices = np.arange(X_train.shape[0])
+shuffled_indices = np.random.permutation(indices)
+
+X_train = X_train[shuffled_indices]
+Y_train = Y_train[shuffled_indices]
 
